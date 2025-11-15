662cd041578529cb877f687c47d4d1f8b7cd5cf5
Elliott Feltham <elliottfeltham98@gmail.com>
2025-11-15 16:09:39 +0000

    Initial Commit

---
+++ scripts/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+const SPEED = 500.0
+
+
+func _physics_process(delta: float) -> void:
+
+	# A normalised vector in the direction you press
+	var input_vector = Input.get_vector("move_left", 
+										"move_right", 
+										"move_up", 
+										"move_down") 
+	
+	# Multiply that vector by the speed you want to move
+	velocity = input_vector * SPEED
+	
+	# Move and slide multiplies by delta internally
+	move_and_slide()
