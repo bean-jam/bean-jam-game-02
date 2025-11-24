4a9ce3863e31e98119c203bf61510a3c11ecc9a8
Elliott Feltham <elliottfeltham98@gmail.com>
2025-11-24 15:27:47 +0000

    Refactor MeleeWeapon with documentation

--- scripts/weapons/MeleeWeapon.gd
+++ scripts/weapons/MeleeWeapon.gd
@@ -1,13 +1,17 @@
 extends Node2D
 class_name MeleeWeapon
-# This weapon automatically attacks with the entities base stats once the cooldown
-# has ended.
+## This weapon automatically attacks with the entities base stats once the 
+## cooldown has ended.
 
-# Extend how long your weapon hitbox stays active for.
-@export var hitbox_duration: float = 0.2
+## Assign a BaseEntity as the holder of this weapon.
 @export var holder: BaseEntity
+## Extend how long your weapon hitbox stays active for.
+@export var hitbox_duration: float = 0.2
 
+## Find the HitboxComponent which should be a direct child of MeleeWeapon.
 @onready var hitbox: HitboxComponent = $HitboxComponent
+
+## Attack cooldown
 var cooldown: float = 0.0
 
 func _ready() -> void:
@@ -15,14 +19,19 @@ func _ready() -> void:
 		print("Assign a holder for this weapon")
 		return
 
+## Runs every physics process to ensure holder is assigned and to reduce cooldown
+## between attacks.
 func _physics_process(delta: float) -> void:
 	if holder == null or not is_instance_valid(holder):
 		return
 
-	cooldown -= delta
+	cooldown = max(0.0, cooldown - delta)
 	if cooldown <= 0.0:
 		_perform_attack()
 
+
+## Called once the attack cooldown reaches 0
+## The function initialises the hitbox and then enables it for the set duration.
 func _perform_attack() -> void:
 	cooldown = holder.stats.attack_cooldown
 
@@ -32,6 +41,8 @@ func _perform_attack() -> void:
 	# Enable hitbox for a short timed window
 	_enable_hitbox_for(hitbox_duration)
 
+
+## Enables a hitbox for the alloted duration period.
 func _enable_hitbox_for(duration: float) -> void:
 	hitbox.monitoring = true
 	await get_tree().create_timer(duration).timeout
