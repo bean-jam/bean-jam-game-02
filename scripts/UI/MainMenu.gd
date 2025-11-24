f9048e51f4794581e179b5f0ef5e46e9012631da
Elliott Feltham <elliottfeltham98@gmail.com>
2025-11-24 08:15:51 +0000

    Refactor main menu into separate scene

--- scripts/UI/MainMenu.gd
+++ scripts/UI/MainMenu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var next_wave_button = $MainMenu/VBoxContainer/Button
+@onready var next_wave_button = $MainMenuContainer/VBoxContainer/Button
 
 func _on_button_pressed() -> void:
 	SignalBus.new_game_started.emit()
